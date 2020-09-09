5e94772a207c16d1755fd7ea3e03ae8e45574a8f
Jefferson <jefferestrada32@gmai.com>
2020-09-09 16:33:05 -0500

    Se creo el carrito de compras

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// Se ejecuta despues de que carga todo el html
+(function() {
+    'use strict';
+
+    var regalo = document.getElementById('regalo');
+    document.addEventListener('DOMContentLoaded', function() {
+        // Datos de usuarios
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var emial = document.getElementById('email');
+
+        // Campos pases
+        var pase_dia = document.getElementById('pase-dia');
+        var pase_completo = document.getElementById('pase-completo');
+        var pase_dosDias = document.getElementById('pase-dos-dias');
+
+        // Botones y divs
+        var calcular = document.getElementById('calcular');
+        var error_div = document.getElementById('error');
+        var btnRegistro = document.getElementById('btnRegistro');
+        var resultado = document.getElementById('lista-productos');
+        var suma = document.getElementById('suma-total');
+
+        // Extras
+        var cantStickers = document.getElementById('sticker-evento');
+        var cantCamisas = document.getElementById('camisa-evento');
+
+        calcular.addEventListener('click', calcularTotal);
+
+        function calcularTotal(event) {
+            event.preventDefault();
+            if (regalo.value == '') {
+                alert('Debes de elegir un regalo');
+                regalo.focus();
+            } else {
+                if (pase_dia.value == 0 && pase_dosDias.value == 0 && pase_completo.value == 0) {
+                    alert('Debe de elegir al menos un pase para el evento')
+                } else {
+                    var totalPagar = (pase_dia.value * 30) + (pase_dosDias.value * 45) + (pase_completo.value * 50) + ((cantCamisas.value * 10) * 0.93) + (cantStickers.value * 2);
+
+                    var listaProductos = [];
+
+                    if (pase_dia.value >= 1) {
+                        listaProductos.push('x' + pase_dia.value + ' Pase por día');
+                    }
+
+                    if (pase_dosDias.value >= 1) {
+                        listaProductos.push('x' + pase_dosDias.value + ' Pase por dos días');
+                    }
+
+                    if (pase_completo.value >= 1) {
+                        listaProductos.push('x' + pase_completo.value + ' Pase completo');
+
+                    }
+
+                    if (cantCamisas.value >= 1) {
+                        listaProductos.push('x' + cantCamisas.value + ' Camisas');
+                    }
+
+                    if (cantStickers.value >= 1) {
+                        listaProductos.push('x' + cantStickers.value + ' Stickers');
+                    }
+
+                    resultado.innerHTML = '';
+                    for (var i = 0; i < listaProductos.length; i++) {
+                        resultado.innerHTML += listaProductos[i] + '<br/>'
+                    }
+                    suma.innerHTML = totalPagar.toFixed(2) + ' COP';
+                }
+            }
+        }
+
+
+    });
+
+})();
